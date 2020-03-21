72b429f40d4103186ed4856feb435dab9e6a4769
Jason McGloin <jason.mcgloin@gmail.com>
2020-03-20 23:57:20 -0400

    Disables sort button when button's sort is current sort method

--- priorityze/priorityze-client/src/pages/UserPage.js
+++ priorityze/priorityze-client/src/pages/UserPage.js
@@ -6,9 +6,9 @@ import GoalsList from '../components/GoalsList';
 
 class UserPage extends Component {
 	state = {
-		sortBy: "sortByImportance",
-		data: data,
-		sortedData: data
+		sortBy: "sortByDeadline",
+		goals: [],
+		sortedGoals: []
 	}
 	addGoal = () => {
 
@@ -26,7 +26,7 @@ class UserPage extends Component {
 	renderButtons = () => {
 		const sorts = ["sortByAlphabetic", "sortByImportance", "sortByDeadline"]
 		return Array(sorts.length).fill("").map((button,i) => {
-			return <button key={ i } onClick={ () => this[sorts[i]]() } >{ sorts[i].split("By")[1] }</button>
+			return <button key={ i } onClick={ () => this[sorts[i]]() } disabled={ this.state.sortBy === sorts[i] } >{ sorts[i].split("By")[1] }</button>
 		})
 	}
 	renderGoals = () => {
@@ -38,41 +38,50 @@ class UserPage extends Component {
 					{this.renderButtons()}
 				</div>
 			</div>
-				<GoalsList goals={ this.state.sortedData } />
+			<GoalsList goals={ this.state.sortedGoals } />
 			</React.Fragment>
 		)
 	}
 	renderNoGoals = () => {
-		
+		return (
+			<React.Fragment>
+				no goals
+			</React.Fragment>
+		)
 	}
 	render() {
 		return (
 			<React.Fragment>
 			User Page
-				{ !!this.state.data.length && this.renderGoals() }
+				{ !!this.state.goals.length && this.renderGoals() }
 			</React.Fragment>
 		)
 	}
 
 	componentDidMount = () => {
-		this[this.state.sortBy]();
+		this.setState({
+			goals: data
+		}, () => this[this.state.sortBy]())
 	}
 
 	sortByDeadline = () => {
 		this.setState({
-			sortedData: this.state.data.sort((a,b) => a.deadline.getTime() - b.deadline.getTime())
+			sortBy: "sortByDeadline",
+			sortedGoals: this.state.goals.sort((a,b) => a.deadline.getTime() - b.deadline.getTime())
 		})
 	}
 
 	sortByAlphabetic = () => {
 		this.setState({
-			sortedData: this.state.data.sort((a,b) => (a.title < b.title) ? -1 : (a.title > b.title) ? 1 : 0)
+			sortBy: "sortByAlphabetic",
+			sortedGoals: this.state.goals.sort((a,b) => (a.title < b.title) ? -1 : (a.title > b.title) ? 1 : 0)
 		})
 	}
 
 	sortByImportance = () => {
 		this.setState({
-			sortedData: this.state.data.sort((a,b) => a.importance - b.importance)
+			sortBy: "sortByImportance",
+			sortedGoals: this.state.goals.sort((a,b) => a.importance - b.importance)
 		})
 	}
 }
