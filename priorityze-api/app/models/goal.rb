923aa3893c8d06c8052e71080003bf57fefe8854
Jason McGloin <jason.mcgloin@gmail.com>
2020-03-21 23:19:16 -0400

    Creates session and registration controllers, sets up routes, cofigures for devise, creates validations

--- priorityze-api/app/models/goal.rb
+++ priorityze-api/app/models/goal.rb
@@ -1,3 +1,7 @@
 class Goal < ApplicationRecord
 	has_many :steps
+	belongs_to :user
+
+	validates :title, :deadline, presence: true
+	validates :title, uniqueness: true
 end
--- priorityze-api/config/routes.rb
+++ priorityze-api/config/routes.rb
@@ -1,7 +1,26 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users,
+ 				path: '',
+				path_names: {
+				    sign_in: 'login',
+  				    sign_out: 'logout',
+  				    registration: 'signup'
+  				},
+  				controllers: {
+  				    sessions: 'sessions',
+  				    registrations: 'registrations'
+  				}
+
+  namespace :api do
+  	namespace :v1 do
+  		resource :goal
+  		resource :step
+  	end
+  end
+
 	root to: "home#index"
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
